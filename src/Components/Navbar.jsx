988847f6f4cf14d93f8ca789e4d3a9d6b1eb7798
muhammed <ajferaju9961@gmail.com>
2024-04-24 06:38:58 +0530

    navbar modified

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,18 +5,18 @@ function Navbar() {
     <>
       <div className="fixed top-0 text-white w-full h-16 flex justify-between p-2 items-center px-6 ">
         <img
-          className="w-32"
+          className="w-32 cursor-pointer"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
           alt=""
         />
         <div>
-          <ul className="flex space-x-3 font-extralight">
-            <li>Movies</li>
-            <li>Shows</li>
-            <li>TV shows</li>
+          <ul className="flex space-x-3 font-extralight ">
+            <li className="cursor-pointer hover:text-gray-200">Movies</li>
+            <li className="cursor-pointer hover:text-gray-200">Shows</li>
+            <li className="cursor-pointer hover:text-gray-200">TV shows</li>
           </ul>
         </div>
-        <CgProfile size={24} />
+        <CgProfile size={24} className="cursor-pointer" />
       </div>
     </>
   );
