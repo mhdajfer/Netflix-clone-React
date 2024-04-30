69c50e81efb427d3734d7918d85f822b0ac38b08
muhammed <ajferaju9961@gmail.com>
2024-04-30 18:04:13 +0530

    added trailer comp

--- src/Components/MoviePoster.jsx
+++ src/Components/MoviePoster.jsx
@@ -3,8 +3,9 @@
 import { useState } from "react";
 
 /* eslint-disable no-unused-vars */
-function MoviePoster({ movie, size }) {
+function MoviePoster({ movie, size, handleTrailer }) {
   const [hoverPoster, sethoverPoster] = useState(false);
+
   return (
     <>
       <div className={`w-48 hover:scale-125 mx-1 transition-all ` + size}>
@@ -12,6 +13,7 @@ function MoviePoster({ movie, size }) {
           src={import.meta.env.VITE_IMG_BASIC_URL + movie?.poster_path}
           alt=""
           className=" cursor-pointer hover:z-10  w-full h-full object-cover"
+          onClick={() => handleTrailer(movie?.id)}
         />
       </div>
     </>
--- src/Components/RowComponent.jsx
+++ src/Components/RowComponent.jsx
@@ -1,20 +1,40 @@
 import MoviePoster from "./MoviePoster";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import YouTube from "react-youtube";
 
 // eslint-disable-next-line react/prop-types, no-unused-vars
 function RowComponent({ url, title, size = "", rowSize = "h-[350px]" }) {
   const [movieList, setMovieList] = useState([]);
+  const [videoId, setVideoId] = useState("");
+  const YtOpts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   useEffect(() => {
     getBannerImg(url);
   }, [url]);
-
 
   function getBannerImg(url) {
     axios.get(url + `${import.meta.env.VITE_MOVIE_API_KEY}`).then((res) => {
       setMovieList(res.data.results);
     });
   }
+
+  const handleTrailer = (id) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${
+          import.meta.env.VITE_MOVIE_API_KEY
+        }&language=en-US`
+      )
+      .then((res) => setVideoId(res.data.results[0].key))
+      .catch((err) => console.log("something went wrong", err));
+    console.log(videoId);
+  };
   return (
     <>
       <h1 className="text-white ms-4 font-semibold mb-[-30px]">{title}</h1>
@@ -24,10 +44,16 @@ function RowComponent({ url, title, size = "", rowSize = "h-[350px]" }) {
       >
         <div className="flex flex-nowrap">
           {movieList.map((movie) => (
-            <MoviePoster key={movie?.id} movie={movie} size={size} />
+            <MoviePoster
+              key={movie?.id}
+              movie={movie}
+              size={size}
+              handleTrailer={handleTrailer}
+            />
           ))}
         </div>
       </div>
+      {videoId ? <YouTube videoId={videoId} opts={YtOpts} /> : null}
     </>
   );
 }
